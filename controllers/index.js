0f5dcb77a9629ff09caea22b9b608e7219004df6
JJ <jonathanjablonski94@gmail.com>
2021-05-13 20:25:15 -0500

    Server runs and seeds populate, having issue with get route in controllers

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
-const blogRoutes = require('./blog-routes');
+// const blogRoutes = require('./blog-routes');
 
 router.use('/api', apiRoutes);
+// router.use('/api/posts', blogRoutes);
 
 module.exports = router;
