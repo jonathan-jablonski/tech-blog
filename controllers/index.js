26bf76a0d6295b3743de7a3c5e2f9d0671cb543d
jonathan-jablonski <jonathanjablonski94@gmail.com>
2021-05-16 20:49:51 -0500

    Updates after tutoring

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const adminRoutes = require('./adminRoutes');
-// const blogRoutes = require('./blog-routes');
+const blogRoutes = require('./blog-routes');
 
 router.use('/api', apiRoutes);
 router.use('/', adminRoutes);
-// router.use('/api/posts', blogRoutes);
+router.use('/api/posts', blogRoutes);
 
 module.exports = router;
